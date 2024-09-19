898fce3c8dccb2dfe043ca4c7cddd3cf6d046c84
mohammedasan <mohammedasan1480@gmail.com>
2024-09-19 20:06:40 +0530

    i

--- src/Pages/Content.jsx
+++ src/Pages/Content.jsx
@@ -5,7 +5,7 @@ const Content = () => {
         <div className="h-screen w-screen flex flex-row bg-gray-950 border border-transparent rounded fixed">
         <div className="w-[50%] h-[30%] ml-40 mt-40 flex flex-col bg-gray-950 justify-start items-start px-5 py-3 gap-2">
             <p className="text-2xl text-zinc-100">Hello,</p>
-            <h1 className="text-4xl text-amber-400">I'm Mohammed Asan,</h1>
+            <h1 className="text-4xl text-amber-400">I'm Mohammed Asan,I</h1>
             <h1 className="text-4xl text-zinc-100">Full Stack Developer</h1>
             <p className="text-1xl text-zinc-100">Specializing in creating dynamic, responsive web applications with MongoDB, Express.js, React.js, and Node.js. Passionate about building scalable solutions and optimizing user experiences through modern web technologies.</p>
         </div>
